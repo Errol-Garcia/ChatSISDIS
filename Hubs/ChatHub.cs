9bef7fbfa09f323812d0b0e7cc93c6cf9b76c5ea
Errol Garcia <errolgsilva@gmail.com>
2023-08-28 15:37:14 -0500

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSISDIS.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(/*string room,*/ string user, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", user, message);
+            
+            //await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+
+        /*public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho", $"Alguien se conecto {Context.ConnectionId}");
+        }*/
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatSISDIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
